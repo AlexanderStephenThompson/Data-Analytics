e8f35032afe5cb8e71d93849ab4018e969d18f3e
Alexander S. Thompson <AlexanderStephenThompson@gmail.com>
2024-08-29 09:17:21 -0400

    Added content

---
+++ Data Modeling/Machine Learning/Classification/Titanic Survival Prediction/Snippet.py
@@ -0,0 +1,10 @@
+# Previous testing done with GridSearchCV. Tested with even more models before, but random forest always won.
+# param_grid = {
+#     "n_estimators": [50, 100, 200, 300],
+#     "criterion": ["gini", "entropy"],
+#     "max_depth": [None, 10, 20, 30],
+#     "min_samples_split": [2, 5, 10],
+#     "min_samples_leaf": [1, 2, 4],
+#     "max_features": ["auto", "sqrt", "log2"],
+#     "bootstrap": [True, False]
+# }
